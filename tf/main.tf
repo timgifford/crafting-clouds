e077d1c1c69bcb2debe3da0068487c39c2c899e1
Tim Gifford <tgifford@gmail.com>
2017-04-02 12:20:21 -0500

    fmt tf files

--- tf/main.tf
+++ tf/main.tf
@@ -12,6 +12,7 @@ variable "subnet" {
 
 variable "ami" {
   type = "map"
+
   default = {
     "397713163394" = "ami-0b33d91d"
     "999999999999" = "ami-b63769a1"
@@ -21,22 +22,27 @@ variable "ami" {
 data "aws_caller_identity" "current" {}
 
 resource "aws_instance" "webserver" {
-  ami = "${lookup(var.ami, data.aws_caller_identity.current.account_id)}"
+  ami           = "${lookup(var.ami, data.aws_caller_identity.current.account_id)}"
   instance_type = "t2.micro"
-  key_name = "crafting-clouds"
-  subnet_id = "${var.subnet}"
+  key_name      = "crafting-clouds"
+  subnet_id     = "${var.subnet}"
+
   security_groups = [
-    "${aws_security_group.all_http.id}"]
+    "${aws_security_group.all_http.id}",
+  ]
 }
 
 resource "aws_security_group" "all_http" {
   description = "Static public WebServer"
-  vpc_id = "${var.vpc}"
+  vpc_id      = "${var.vpc}"
+
   ingress {
     from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    to_port   = 80
+    protocol  = "tcp"
+
     cidr_blocks = [
-      "0.0.0.0/0"]
+      "0.0.0.0/0",
+    ]
   }
 }
